87c66565cb62b20e2d09d9011b36cfe2382b173c
shub489 <shubhangupta2c500rki@gmail.com>
2024-09-21 11:49:21 +0530

    multifile upload: only files and not avatar

--- index.js
+++ index.js
@@ -3,11 +3,25 @@ const multer = require("multer")
 
 const app = express()
 
+// { dest: 'uploads/' } => files will be uploaded to uploads folder
 const multerMiddleware = multer({ dest: 'uploads/' })
 
-app.get("/test", (req, res) => {
-  return res.json({
-    message: "working"
+//Single file upload
+// From the frontend/postman, file/image will be uploaded keeping key as "file"
+app.post("/single-upload", multerMiddleware.single("file"), (req, res) => {
+  return res.status(200).json({
+    success: true,
+    message: "Single file upload successfully"
+  })
+})
+
+// Multi file upload
+// Can upload max 3 files at once
+// From the frontend/postman, file/image will be uploaded keeping key as "files"
+app.post("/multi-upload", multerMiddleware.array("files", 3), (req, res) => {
+  return res.status(200).json({
+    success: true,
+    message: "Multi file upload successfully"
   })
 })
 
